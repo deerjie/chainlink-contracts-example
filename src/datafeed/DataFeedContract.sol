8035df9dde78562b69ad216b0edee1d2fb16885b
deer-jay <h117d416@gmail.com>
2025-05-07 16:14:32 +0800

    chainlink使用示例项目初始化

---
+++ src/datafeed/DataFeedContract.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract DataFeedContract {
+    AggregatorV3Interface internal dataFeed;
+
+    constructor() {
+        dataFeed = AggregatorV3Interface(
+		        // 喂价合约地址获取：https://docs.chain.link/data-feeds/price-feeds/addresses?page=1#sepolia-testnet
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
+    }
+
+    function getChainlinkDataFeedLatestAnswer() public view returns (uint256) {
+        // prettier-ignore
+        (
+            /* uint80 roundId */,
+            int256 answer,
+            /*uint256 startedAt*/,
+            /*uint256 updatedAt*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return uint256(answer);
+    }
+}
+//forge install smartcontractkit/chainlink-brownie-contracts --no-commit
